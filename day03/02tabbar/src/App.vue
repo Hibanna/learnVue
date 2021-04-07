c393bd972ad45e0049359454d1626c71ca14adab
Hibanna <2428158037@qq.com>
2021-04-07 17:35:14 +0800

    learnvue17

---
+++ day03/02tabbar/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div id="tab-bar">
+      <div class="tab-bar-item">首页</div>
+      <div class="tab-bar-item">分类</div>
+      <div class="tab-bar-item">购物车</div>
+      <div class="tab-bar-item">我的</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+@import "assets/css/base.css";
+#tab-bar {
+  display: flex;
+  background-color: #e2e2e2;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+.tab-bar-item {
+  height: 49px;
+  flex: 1;
+  text-align: center;
+
+}
+</style>
